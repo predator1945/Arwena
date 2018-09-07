62ce9a9e9212c211a9b358ec399449a43efa3aa3
arwena <a.szuflicki@gmail.com>
2018-09-07 23:35:30 +0200

    video converting microservice

--- backend/convertService/src/index.js
+++ backend/convertService/src/index.js
@@ -4,16 +4,28 @@ const { EventEmmiter } = require('events');
 const mediator = new EventEmmiter();
 const config = require('./config');
 
-console.log('--- Minio Microservice ---');
-console.log('Connecting to queue');
+console.log('--- Converting Video Microservice ---');
+console.log('Connecting to queue...');
 
 let queue = null;
 
 mediator.on('queue.ready', q => {
     console.log('Connected to queue. Connecting to minio...');
     queue = q;
-    
 })
+
+mediator.on('minio.ready', client => {
+    repository.connect(client, queue)
+        .then(repo => {
+            console.log('Minio client connected. Starting worker...');
+        });
+
+        return worker.start({
+            port: config.serverSettings.port,
+            repo,
+            queue
+        })
+});
 
 config.queue.connect(config.qSettings, mediator);
 
