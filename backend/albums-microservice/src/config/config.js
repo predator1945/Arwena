d5622d0ca779e8bc93e2c2c7af40f38f72fd24c5
arwena <a.szuflicki@gmail.com>
2018-10-16 01:13:58 +0200

    albums-microservice finished

---
+++ backend/albums-microservice/src/config/config.js
@@ -0,0 +1,13 @@
+const dbSettings = {
+    db: 'arwena',
+    user: process.env.DB_USER || 'user',
+    pass: process.env.DB_PASS || 'pass',
+    url: process.env.DB_SERVER || 'localhost:27017'
+}
+
+const serverSettings = {
+    port: process.env.PORT || 3001,
+    ssl: require('./ssl')
+  }
+  
+  module.exports = Object.assign({}, { dbSettings, serverSettings })
---
+++ backend/containers/mongo/create.sh
@@ -0,0 +1,1 @@
+$ docker run --name mongodb -d mongo MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=pass
