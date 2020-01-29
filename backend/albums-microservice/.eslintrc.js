fb64de3cc68e36992535d7afb0ef4e93bd4868f2
Arek <a.szuflicki@gmail.com>
2020-01-29 23:17:59 +0100

    Deleted albums microservice

--- backend/albums-microservice/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    "extends": "standard",
-    "rules": {
-        "indent": ["error", "tab"],
-        "no-tabs": 0
-      }
-};
--- backend/albums-microservice/src/config/ssl/index.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-
-module.exports = {
-	key: fs.readFileSync(`${__dirname}/key.pem`),
-	cert: fs.readFileSync(`${__dirname}/cert.pem`)
-}
