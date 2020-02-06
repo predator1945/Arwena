ca43ff53469ce309c3a052cb2dfc02136fecc9ea
Arek <a.szuflicki@gmail.com>
2020-02-06 17:36:35 +0100

    Create react app

---
+++ webapp/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NowPlayingBar from './now-playing-bar/NowPlayingBar';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      
+      <NowPlayingBar />
+    </div>
+  );
+}
+
+export default App;
